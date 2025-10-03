6bd12ca6b702f7e7a035b652295e2fa8fd00eb0b
RafaPRG <rafaponcergmg1291@gmail.com>
2025-10-03 14:03:19 -0300

    core e testes feitos

--- src/domain/use-cases/RegisterUser.ts
+++ src/domain/use-cases/RegisterUser.ts
@@ -8,6 +8,11 @@ export class RegisterUser {
   constructor(private userRepository: IUserRepository) {}
 
   async execute(id: string, name: string, email: string, password: string): Promise<User> {
+    const existingUser = await this.userRepository.findByEmail(email);
+    if (existingUser) {
+      throw new Error("User with this email already exists");
+    }
+
     const user = User.create(id, Name.create(name), Email.create(email), Password.create(password));
     await this.userRepository.save(user);
     return user;
